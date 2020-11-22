aaf9f32fad25a19e05cde90849f2438ba315a4a2
Marius-Adam <marius.c.adam@gmail.com>
2020-11-22 17:10:19 +0000

    Updated authenticate service links

--- src/api/Authentication.js
+++ src/api/Authentication.js
@@ -1,21 +1,24 @@
 import axios from "axios";
 
-const TMDB_API = process.env.REACT_APP_TMDB_API_KEY;
-const TOKEN_URL = process.env.REACT_APP_TMDB_NEW_TOKEN_URL;
-const AUTHENTICATE_URL = process.env.REACT_APP_TMDB_AUTHENTICATE_URL;
-const VALIDATE_TOKEN_URL = process.env.REACT_APP_TMDB_VALIDATE_TOKEN;
+const TMDB_KEY = "6bec9ad5a46f7fe6699df4bba6a319fc";
+const TOKEN_URL =
+  "https://api.themoviedb.org/3/authentication/token/new?api_key=";
+const AUTHENTICATE_URL = "https://www.themoviedb.org/authenticate/";
+const VALIDATE_TOKEN_URL =
+  "https://api.themoviedb.org/3/authentication/token/validate_with_login?api_key=";
 
 export function getAccessToken() {
-  return axios.get(`${TOKEN_URL}${TMDB_API}`).then((token) => token.data);
+  return axios.get(`${TOKEN_URL}${TMDB_KEY}`).then((token) => token.data);
 }
 
 export function redirectSignIn(token) {
-  window.location.href = `${AUTHENTICATE_URL}${token}?redirect_to=http://localhost:3000/login`;
+  window.location.href = `${AUTHENTICATE_URL}${token}?redirect_to=https://nervous-khorana-7457e2.netlify.app/login`;
 }
+//http://localhost:3000/login
 
 export function validateToken(username, password, request_token) {
   return axios
-    .post(`${VALIDATE_TOKEN_URL}${TMDB_API}`, {
+    .post(`${VALIDATE_TOKEN_URL}${TMDB_KEY}`, {
       username,
       password,
       request_token,
